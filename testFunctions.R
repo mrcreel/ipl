763ad369790e7c591a6ce4a5b549a0adc7fc0655
Michael R Creel <michaelrcreel@gmail.com>
2017-01-08 16:57:36 -0600

    initial commit

--- testFunctions.R
+++ testFunctions.R
@@ -4,8 +4,28 @@ for(i in 1:nrow(t.venues.home)){
   row.t <- as.character(row['team'])
   row.v <- as.character(row['venue'])
   
-  t.match.bat.stats %>%
+  home <-t.match.bat.stats %>%
     filter_(~season == row.s,
             ~venue == row.v,
-            ~team == row.t)
+            ~team == row.t) %>%
+    group_by(season, team) %>%
+    select(-match.id) %>%
+    summarise(balls = sum(balls),
+                  runs = sum(runs),
+                  bat.4s = sum(bat.4s),
+                  bat.6s = sum(bat.6s)) %>%
+    mutate(strike.rate = runs/balls * 100)
+  
+  away <-t.match.bat.stats %>%
+    filter_(~season == row.s,
+            ~venue != row.v,
+            ~team == row.t) %>%
+    group_by(season, team) %>%
+    select(-match.id) %>%
+    summarise(balls = sum(balls),
+                  runs = sum(runs),
+                  bat.4s = sum(bat.4s),
+                  bat.6s = sum(bat.6s)) %>%
+    mutate(strike.rate = runs/balls * 100)
+  
 }
